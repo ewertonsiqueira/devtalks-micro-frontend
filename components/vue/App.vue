0aa0b908d2b038a09bf11c2e5c9211e4349fb349
Ewerton Siqueira <ewertoncostaxx@hotmail.com>
2025-01-28 20:19:42 -0300

    adicionando funcao

--- components/vue/App.vue
+++ components/vue/App.vue
@@ -13,7 +13,17 @@ export default {
   data: () => ({
     productList: []
   }),
-  mounted() {},
-  methods: {}
+  mounted() {
+    this.searchProducts();
+  },
+  methods: {
+    searchProducts() {
+      fetch('http://localhost:3001/estoque')
+        .then(response => response.json())
+        .then(data => {
+          this.productList = data;
+        });
+    }
+  }
 }
 </script>
